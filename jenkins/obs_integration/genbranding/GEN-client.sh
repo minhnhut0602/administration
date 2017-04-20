50ae023d4b4b98a230c40bd581fd18ea9c4e6a0c
Juergen Weigert <jw@owncloud.com>
2017-04-20 19:46:43 +0200

    refactored RUN.sh into GEN-*.sh examples.

---
+++ jenkins/obs_integration/genbranding/GEN-client.sh
@@ -0,0 +1,22 @@
+themetar=testpilotcloud.tar.xz
+# themetar=jwfablabnbg.tar.xz
+# themetar=ownCloud.tar.xz
+
+sh prepare_tarball.sh testdata/owncloudclient-2.3.2git.tar.bz2 testdata/customer-themes/$themetar testdata/tp_tmplvars.sh
+# Please deprecate use of prjconf variables
+sh fetch_prjconf_vars.sh isv:ownCloud:devel:Qt562 >> testdata/tp_tmplvars.sh
+
+test -z "${BUILD_NUMBER}" && BUILD_NUMBER=$(date +%y%m%d)	# default when not running in jenkins
+
+. testdata/tp_tmplvars.sh
+cat <<EOF >> testdata/tp_tmplvars.sh
+BUILD_NUMBER="${BUILD_NUMBER}"
+VENDOR="ownCloud"
+PACKAGE_NAME_PREFIX="ocqt562"
+OC_QT_VERSION="5.6.2"
+OC_QT_ROOT="/opt/${VENDOR}/qt-${OC_QT_VERSION}"
+CLIENT_ROOT="/opt/${VENDOR}/${APPLICATION_SHORTNAME}"
+EOF
+
+sh prepare_package.sh ../templates/client/v$BASEVERSION out testdata/tp_tmplvars.sh
+
---
+++ jenkins/obs_integration/genbranding/GEN-deps.sh
@@ -0,0 +1,8 @@
+cat <<EOF > deps_tmplvars.sh
+VENDOR="ownCloud"
+PACKAGE_NAME_PREFIX="ocqt562"
+OC_QT_VERSION="5.6.2"
+OC_QT_ROOT="/opt/${VENDOR}/qt-${OC_QT_VERSION}"
+EOF
+
+sh prepare_package.sh ../templates/client-deps/qt-${OC_QT_VERSION} out testdata/tp_tmplvars.sh
