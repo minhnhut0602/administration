99640aded9a0e89d799ce9785a4b11219a3a0917
Juergen Weigert <jw@owncloud.com>
2017-04-20 20:12:55 +0200

    fixed variable expansions...

--- jenkins/obs_integration/genbranding/GEN-client.sh
+++ jenkins/obs_integration/genbranding/GEN-client.sh
@@ -9,13 +9,15 @@ sh fetch_prjconf_vars.sh isv:ownCloud:devel:Qt562 >> testdata/tp_tmplvars.sh
 test -z "${BUILD_NUMBER}" && BUILD_NUMBER=$(date +%y%m%d)	# default when not running in jenkins
 
 . testdata/tp_tmplvars.sh
+# Escape the $-interpolation expansions here. We could expand some of the variables here, 
+# but not all. Later expansion is safer.
 cat <<EOF >> testdata/tp_tmplvars.sh
 BUILD_NUMBER="${BUILD_NUMBER}"
 VENDOR="ownCloud"
 PACKAGE_NAME_PREFIX="ocqt562"
 OC_QT_VERSION="5.6.2"
-OC_QT_ROOT="/opt/${VENDOR}/qt-${OC_QT_VERSION}"
-CLIENT_ROOT="/opt/${VENDOR}/${APPLICATION_SHORTNAME}"
+OC_QT_ROOT="/opt/\${VENDOR}/qt-\${OC_QT_VERSION}"
+CLIENT_ROOT="/opt/\${VENDOR}/\${APPLICATION_SHORTNAME}"
 EOF
 
 sh prepare_package.sh ../templates/client/v$BASEVERSION out testdata/tp_tmplvars.sh
--- jenkins/obs_integration/genbranding/GEN-deps.sh
+++ jenkins/obs_integration/genbranding/GEN-deps.sh
@@ -1,8 +1,9 @@
-cat <<EOF > deps_tmplvars.sh
+cat <<EOF > testdata/deps_tmplvars.sh
 VENDOR="ownCloud"
 PACKAGE_NAME_PREFIX="ocqt562"
 OC_QT_VERSION="5.6.2"
-OC_QT_ROOT="/opt/${VENDOR}/qt-${OC_QT_VERSION}"
+OC_QT_ROOT="/opt/\${VENDOR}/qt-\${OC_QT_VERSION}"
 EOF
 
-sh prepare_package.sh ../templates/client-deps/qt-${OC_QT_VERSION} out testdata/tp_tmplvars.sh
+. testdata/deps_tmplvars.sh
+sh prepare_package.sh ../templates/client-deps/qt-${OC_QT_VERSION} out testdata/deps_tmplvars.sh
